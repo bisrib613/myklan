3e57376d3e95f0747dbc1ec808cba3fa258d2f60
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-01-31 02:59:45 +0700

    Update klan.js

--- klan.js
+++ klan.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 	atOptions = {
 		'key' : 'ec7cad2f974765fe73f39986fa5879ab',
 		'format' : 'iframe',
@@ -7,4 +7,4 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/ec7cad2f974765fe73f39986fa5879ab/invoke.js"></scr' + 'ipt>');
-</script>
+
