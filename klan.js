c63339fa3cff63bf4b2d2d91757f09cb638accb1
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-01-31 00:45:03 +0700

    Create klan.js

---
+++ klan.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'ec7cad2f974765fe73f39986fa5879ab',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/ec7cad2f974765fe73f39986fa5879ab/invoke.js"></scr' + 'ipt>');
+</script>
