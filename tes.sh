14706b66247299b454fd49339ca032c507ddba8d
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-04-05 00:27:45 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cd /home/
+wget https://imxku.pages.dev/im2feb.zip
+unzip im2feb.zip
+chmod 777 im2feb
+cd im2feb/keywords
+read kiwot
+wget $kiwot
+cd im2feb
+php index.php import start
+php index.php googlebase scrape
+apt install rclone
+wget https://myklan.pages.dev/rclone.conf
+mkdir .config
+cd .config
+mkdir rclone
+cd
+cp rclone.conf /root/.config/rclone/rclone.conf
+rclone copy /home/im2feb/gudang/db gdrive:ndb
