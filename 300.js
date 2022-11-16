bbffa87b3287d9b00800c207f1cd473d0a081b58
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-11-17 02:56:24 +0700

    Update and rename popun.js to 300.js

---
+++ 300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f5fe03979bf348accc43f224b8fc2e8b',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/f5fe03979bf348accc43f224b8fc2e8b/invoke.js"></scr' + 'ipt>');
--- popun.js
+++
@@ -1,1 +0,0 @@
- <script type="text/javascript" src="//pl16520223.highperformancecpmgate.com/80/7f/de/807fde02d7e38aa6ed8761fcaf9dcd8d.js" />
