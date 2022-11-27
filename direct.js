c1c19694f03df535a768a9767c7840b5da70cd1c
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-11-28 04:57:52 +0700

    Update direct.js

--- direct.js
+++ direct.js
@@ -11,6 +11,6 @@
 	    $(this).removeClass('xepo_ads');
 
 	    //ubah google dengan ads direct link
-	    window.open('https://www.trustedcpmrevenue.com/qg8q3jgz?key=12b89a93dd3d80c6a8f239fb821bc4b0', '_blank');
+	    window.open('https://www.highperformancecpmgate.com/qg8q3jgz?key=12b89a93dd3d80c6a8f239fb821bc4b0', '_blank');
 	});
 
