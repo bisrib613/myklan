67556f6b35604f744698f0b83aa9320bf51cbc88
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-11-28 04:47:39 +0700

    Create 300x50.js

---
+++ 300x50.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '49473eae5d533f2562f9233cad594292',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/49473eae5d533f2562f9233cad594292/invoke.js"></scr' + 'ipt>');
