6c3c00c6898101388a64fc8f867b7f14a8339496
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2023-07-18 02:53:30 +0700

    Update 728.js

--- 728.js
+++ 728.js
@@ -1,8 +1,8 @@
-atOptions = {
+tOptions = {
 		'key' : 'ea0a5950f4881bb7a66bae751372a5b1',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/ea0a5950f4881bb7a66bae751372a5b1/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/ea0a5950f4881bb7a66bae751372a5b1/invoke.js"></scr' + 'ipt>');
