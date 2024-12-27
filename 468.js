90aa15dbef091b62398e0060333fcffdb94bb8a3
alvianamr <89772807+bisrib613@users.noreply.github.com>
2024-12-28 04:01:31 +0700

    Create 468.js

---
+++ 468.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'ec7cad2f974765fe73f39986fa5879ab',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/ec7cad2f974765fe73f39986fa5879ab/invoke.js"></scr' + 'ipt>');
