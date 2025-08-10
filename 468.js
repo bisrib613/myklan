5875113de76f60cf95ce3990e76624a196db137a
alvianamr <89772807+bisrib613@users.noreply.github.com>
2025-08-11 01:02:20 +0700

    Update 468.js

--- 468.js
+++ 468.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 468,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/ec7cad2f974765fe73f39986fa5879ab/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/ec7cad2f974765fe73f39986fa5879ab/invoke.js"></scr' + 'ipt>');
